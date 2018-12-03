c99361d4716d6f5447cf3a177295cb50a3695a89
GuyCo <co.guycohen@gmail.com>
2018-12-03 14:56:20 +0200

    dogCard commit befor dog model

---
+++ app/src/main/java/dtg/dogretriever/Presenter/DogCardAdapter.java
@@ -0,0 +1,67 @@
+package dtg.dogretriever.Presenter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import dtg.dogretriever.Model.Dog;
+import dtg.dogretriever.R;
+
+public class DogCardAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Dog> dogArrayList;
+    private LayoutInflater inflater;
+    private ViewHolder viewHolder;
+
+    public DogCardAdapter(Context context, ArrayList<Dog> dogArrayList) {
+        this.context = context;
+        this.dogArrayList = dogArrayList;
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+       return dogArrayList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dogArrayList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.scanner_dog_layout,
+                    parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.nameTextView = convertView.findViewById(R.id.scanner_dog_name);
+            viewHolder.sizeTextView =  convertView.findViewById(R.id.scanner_dog_size);
+            viewHolder.colorTextView =  convertView.findViewById(R.id.scanner_dog_color);
+            viewHolder.breedTextView =  convertView.findViewById(R.id.scanner_dog_breed);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        viewHolder.nameTextView.setText(dogArrayList.get(position).());
+        return convertView;
+        }
+
+    static class ViewHolder {
+        private TextView nameTextView;
+        private TextView sizeTextView;
+        private TextView colorTextView;
+        private TextView breedTextView;
+    }
+}
---
+++ app/src/main/java/dtg/dogretriever/Presenter/ScannerActivity.java
@@ -0,0 +1,14 @@
+package dtg.dogretriever.Presenter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import dtg.dogretriever.R;
+
+public class ScannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner);
+    }
+}
